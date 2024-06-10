e6eaacdfe4370f18295d2bac17bc95a7719dc0b6
Uaná Gasparim <121419133+AnnWann@users.noreply.github.com>
2024-06-10 00:08:26 -0300

    optimized tokenize

--- tokenize.js
+++ tokenize.js
@@ -1,73 +1,77 @@
 const crypto = require("crypto");
-const { regex_tokens, regex_stopping_signs } = require("regexCodes.js");
+const { regex_tokens, regex_stopping_signs as stopping_signs } = require("regexCodes.js");
 
 let idTable  = new Map(); 
 
 function tokenize(code){
-  const lexemes = [];
+  const tokens = [];
   let current_char = 0;
-  console.log(code.length)
 
-  while(current_char < code.length){
-    const char = code[current_char];
+  const regex_s_signs = stopping_signs.keys, s_signs_size = stopping_signs.size;
 
-    if(empty_space.test(char)){
-      current_char++;
-      continue;
-    }
-
-    if(letters_numbers.test(char)){
-      let lex = "";
-      while(letters_numbers.test(code[current_char])){
-        if(current_char >= code.length) break;
-        lex += code[current_char++];
+  let i = 0;
+  while(i < code_length){
+    for(let q = 0; q < s_signs_size; q++){
+      const s_sign_regex = regex_s_signs[q];
+      if(s_sign_regex.test(code[i]){
+        const lex = getLexeme(i, code, s_sign_name);
+        if(!lex.value){
+          i += lex.iterations;
+          continue;
+        }
+        tokens.add(makeToken(lex.value));
+        i += lex.iterations;
+        continue;
       }
-      lexemes.push(makeToken(lex));
-      continue;
     }
-
-    if(punctuation.test(char)){
-      lexemes.push(makeToken(lex));
-      continue;
-    }
-
-    if(double_quote.test(char)){
-      let lex = char;
-      current_char++;
-      while(!double_quote.test(code[current_char]))
-        lex += code[current_char++];
-      lexemes.push(makeToken);
-      continue;
-    }
-
-    if(single_quote.test(char)){
-      let lex = char;
-      current_char++;
-      while(!single_quote.test(code[current_char]))
-        lex += code[current_char++];
-      
-      lexemes.push(makeToken);
-      continue;
-    }
-
-    if(m_comment_e.test(lex)){
-      while(!m_comment_s.test(code[current_char++]));
-      continue;
-    }
-
-    if(s_comment.test(lex)){
-      while(!line_b.test(code[current_char++]));
-      continue;
-    }
-
-    let lex = char;
-    current_char++
-    while(operators.test(code[current_char]))
-      lex += code[current_char++];
-    
-    lexemes.push(makeToken);
   }
-  return lexemes; 
+}
+
+function getLexeme(i, code, s_sign_regex){
+  const s_sign_name = stopping_signs.get(s_sign_regex);
+  if(!s_sign_name) throw new Error('unidentified regular expression');
+  switch(s_sign_name){
+    case 'empty space':
+      return { value: undefined, iterations: 1};
+    case 'punctuation':
+      return { value: code[i], iterations: 1};
+    case 'single quote':
+      const q = i+2;
+      if(!s_sign_regex.test(code[q]) throw new Error('lexical error - unclosed single quote');
+      return { value: code.slice(i, q + 1), iterations: 2};
+    case 'operators':
+      let q = i+1;
+      while(q < 2){
+        if(!s_sign_regex.test(code[q])) break;
+        q++
+      }
+      return { value: code.slice(i, q + 1), iterations: q - i};
+    case 'letters and numbers' |'double quote' | 'm_comment_e' | 'line_b':
+       return buildCompoundLexeme(i, code, s_sign_regex);
+    default:
+     throw new Error('unidentified regular expression');
+  }
+}
+
+function buildCompoundLexeme(i, code, predicate){
+  const end = getEndOfLexeme(i, code, predicate);
+  const s_sign_name = stopping_signs.get(s_sign_regex);
+  switch(s_sign_name){
+    case 'letters and numbers' |'double quote':
+      return { value: code.slice(i, end + 1), iterations: end - i};
+    case 'm_comment_e' | 'line_b':
+       return { value: undefined, iterations: end - i};
+  }
+}
+
+function getEndOfLexeme(i, code, predicate){
+  let q = i+1;
+    while(q < code.length){
+      if(predicate.test(code[q])){
+        return q;
+      }
+    }
+  throw new Error('lexical error - missing symbol: ' + predicate);
 }
 
 function setId(lex){
@@ -79,24 +83,13 @@ function setId(lex){
 
 function makeToken(lex){
   const s = regex_tokens.size, token_rules = regex_tokens.keys;
-  for(int i = 0; i < s; i++)
+  for(let i = 0; i < s; i++)
       if(token_rules[i].test(lex)){
         const token_type = regex_tokens.get(token_rules[i]);
         return token_type == 'identifier' ? setId(lex) : { value: lex, type: token_type }; 
       }; 
   return { value: lex, type: 'ERROR'};
 }
-
-const empty_space = /\s/;
-const letters_numbers = /[A-Za-z_0-9_]/;
-const punctuation = /\{|\}|\(|\)|\[|\]|;|.|,|:/;
-const double_quote = /"/;
-const single_quote = /'/;
-const m_comment_s = /\/\*/;
-const m_comment_e = /\*/;
-const s_comment = /\/\//;
-const line_b = /\n/;
-const operators = /=|!|<|>|\+|-|\*\/|>|<|\||&/;
 
 function createSHA256Hash(inputString) {
   const hash = crypto.createHash('sha256');
