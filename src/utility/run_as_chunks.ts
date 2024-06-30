de1cf679ce096a45a94a47198446db6d9d48fa9c
Uaná Gasparim <uanabgasparim@gmail.com>
2024-06-29 23:20:02 -0300

    implemented run_as_chunks

---
+++ src/utility/run_as_chunks.ts
@@ -0,0 +1,23 @@
+
+async function run_as_chunks<B extends Array<any>, T>(
+  thisField: T, 
+  buffer: B, 
+  predicate: (size: number) => boolean, 
+  operand: (thisField: T) => T, 
+  join: (left: T, right: T) => T, 
+  split_rule?: (buffer: B) => B[]): Promise<T>
+{
+
+  const size = buffer.length;
+
+  if(buffer.length <= 0) return thisField;
+
+  if(!predicate(size)) return operand(thisField);
+
+  const [left, right] = split_rule ? split_rule(buffer) : [buffer.slice(0, size/2), buffer.slice(size/2, size - 1)];
+
+  const left_result = await run_as_chunks(thisField, left, predicate, operand, join, split_rule);
+  const right_result = await run_as_chunks(thisField, right, predicate, operand, join, split_rule);
+  
+  return join(left_result, right_result);
+}
